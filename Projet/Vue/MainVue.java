cbc4f7e91089c0c99b9dbf27785799eea08cac78
DeroeuxH <hugo.deroeux@gmail.com>
2022-05-18 03:06:14 +0200

    2.0 - nouvelle vue

--- Projet/Vue/MainVue.java
+++ Projet/Vue/MainVue.java
@@ -1,91 +1,220 @@
 package Vue;
 
 import javax.swing.*;
+
 import java.awt.*;
 import Model.Hotel;
 import Controller.Ctrl;
 
 public class MainVue extends Ctrl {
 
-    public static void main(String Args[]) {
+    // Hotel de test
+    Hotel hotel = new Hotel("chezmoi", "le bahut");
 
-        // Hotel de test
-        Hotel hotel = new Hotel("chezmoi", "le bahut");
-        // Chaque chambre sera approvisionné de 10qté
-        // Pour chaque qté donné à extra, la supprimer de produit
-        hotel.addProduit("Cola", 3, 1000);
-        hotel.addProduit("Bière", 4, 10000);
-        hotel.addProduit("Eau gazeuse", 2, 2000);
-        // Hotel de test
+    // Main Frame
+    JFrame mainJFrame = new JFrame("Menu Principal");
+    // MainPanel
+    JPanel mainJPanel = (JPanel) mainJFrame.getContentPane();
+    // création des boutons
+    JButton chambre = new JButton("Chambre");
+    JButton reservation = new JButton("Reservation");
+    JButton sejour = new JButton("Sejour");
+    JButton client = new JButton("Client");
+    JButton personnel = new JButton("Personnel");
+    JButton retour = new JButton("Retour en arrière");
 
-        // Main Frame
-        JFrame mainJFrame = new JFrame("Menu Principal");
+    public MainVue() {
+
         mainJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainJFrame.setSize(new Dimension(1280, 720));
         mainJFrame.setLocationRelativeTo(null);
         mainJFrame.setVisible(true);
 
-        // MainPanel
-        JPanel mainJPanel = (JPanel) mainJFrame.getContentPane();
-        mainJPanel.setLayout(new GridLayout(0, 2, 5, 40));
+        mainJPanel.setLayout(new GridLayout(5, 1, 5, 40));
 
-        // Chambre
-        JButton addChambreButton = new JButton("Ajouter Chambre");
-        addChambreButton.addActionListener((e) -> addChambreButtonClicked(e, hotel));
-        mainJPanel.add(addChambreButton);
-        JButton modChambreButton = new JButton("Modifier Chambre");
-        modChambreButton.addActionListener((e) -> modChambreButtonClicked(e, hotel));
-        mainJPanel.add(modChambreButton);
+        // Ajout des boutons au menu
+        mainJPanel.add(client);
+        mainJPanel.add(chambre);
+        mainJPanel.add(reservation);
+        mainJPanel.add(sejour);
+        mainJPanel.add(personnel);
+        // ajout des contrôleurs sur les boutons
+        client.addActionListener((e) -> vueClient());
+        chambre.addActionListener((e) -> vueChambre());
+        reservation.addActionListener((e) -> vueReservation());
+        sejour.addActionListener((e) -> vueSejour());
+        personnel.addActionListener((e) -> vuePersonnel());
+    }
 
-        // Client
+    public void nouveauMenu() {
+        mainJPanel.removeAll();
+        mainJPanel.repaint();
+        mainJPanel.revalidate();
+
+        mainJPanel.setLayout(new GridLayout(5, 1, 5, 40));
+
+        // Ajout des boutons au menu
+        mainJPanel.add(client);
+        mainJPanel.add(chambre);
+        mainJPanel.add(reservation);
+        mainJPanel.add(sejour);
+        mainJPanel.add(personnel);
+        // ajout des contrôleurs sur les boutons
+        client.addActionListener((e) -> vueClient());
+        chambre.addActionListener((e) -> vueChambre());
+        reservation.addActionListener((e) -> vueReservation());
+        sejour.addActionListener((e) -> vueSejour());
+        personnel.addActionListener((e) -> vuePersonnel());
+    }
+
+    public void vueClient() {
+        // reset du JPanel
+        mainJPanel.removeAll();
+        mainJPanel.repaint();
+        mainJPanel.revalidate();
+        mainJPanel.setLayout(new GridLayout(3, 1, 5, 40));
+        //
         JButton addClientButton = new JButton("Ajouter Client");
-        addClientButton.addActionListener((e) -> addClientButtonClicked(e, hotel));
-        mainJPanel.add(addClientButton);
         JButton delClientButton = new JButton("Supprimer Client");
-        delClientButton.addActionListener((e) -> delClientButtonClicked(e, hotel));
+
+        mainJPanel.add(addClientButton);
         mainJPanel.add(delClientButton);
+        mainJPanel.add(retour);
+        //
+        addClientButton.addActionListener((e) -> addClientButtonClicked(e, hotel));
+        delClientButton.addActionListener((e) -> delClientButtonClicked(e, hotel));
+        retour.addActionListener((e) -> nouveauMenu());
+    }
 
-        // Receptionniste
-        JButton addReceptionnisteButton = new JButton("Ajouter Réceptionniste");
-        addReceptionnisteButton.addActionListener((e) -> addReceptionnisteButtonClicked(e, hotel));
-        mainJPanel.add(addReceptionnisteButton);
-        JButton delReceptionnisteButton = new JButton("Supprimer Réceptionniste");
-        delReceptionnisteButton.addActionListener((e) -> delReceptionnisteButtonClicked(e, hotel));
-        mainJPanel.add(delReceptionnisteButton);
+    public void vueChambre() {
+        // reset du JPanel
+        mainJPanel.removeAll();
+        mainJPanel.repaint();
+        mainJPanel.revalidate();
+        mainJPanel.setLayout(new GridLayout(3, 1, 5, 40));
+        //
+        JButton addChambreButton = new JButton("Ajouter Chambre");
+        JButton modChambreButton = new JButton("Modifier Chambre");
+        //
+        mainJPanel.add(addChambreButton);
+        mainJPanel.add(modChambreButton);
+        mainJPanel.add(retour);
+        //
+        addChambreButton.addActionListener((e) -> addChambreButtonClicked(e, hotel));
+        modChambreButton.addActionListener((e) -> modChambreButtonClicked(e, hotel));
+        retour.addActionListener((e) -> nouveauMenu());
 
-        // Agent Entretien
-        JButton addAgentButton = new JButton("Ajouter Agent d'entretien");
-        addAgentButton.addActionListener((e) -> addAgentButtonClicked(e, hotel));
-        mainJPanel.add(addAgentButton);
-        JButton delAgentButton = new JButton("Supprimer Agent d'entretien");
-        delAgentButton.addActionListener((e) -> delAgentButtonClicked(e, hotel));
-        mainJPanel.add(delAgentButton);
-        JButton addInterButton = new JButton("Ajouter Intervention pour Agent d'entretien");
-        addInterButton.addActionListener((e) -> addInterButtonClicked(e, hotel));
-        mainJPanel.add(addInterButton);
+    }
+
+    public void vueReservation() {
+        // reset du JPanel
+        mainJPanel.removeAll();
+        mainJPanel.repaint();
+        mainJPanel.revalidate();
+        mainJPanel.setLayout(new GridLayout(4, 1, 5, 40));
 
         // Reservation
         JButton addReservationButton = new JButton("Ajouter Réservation");
-        addReservationButton.addActionListener((e) -> addReservationButtonClicked(e, hotel));
-        mainJPanel.add(addReservationButton);
         JButton modReservationButton = new JButton("Modifier Réservation");
-        modReservationButton.addActionListener((e) -> modReservationButtonClicked(e, hotel));
-        mainJPanel.add(modReservationButton);
         JButton delReservationButton = new JButton("Supprimer Réservation");
-        delReservationButton.addActionListener((e) -> delReservationButtonClicked(e, hotel));
-        mainJPanel.add(delReservationButton);
 
-        // Séjour
+        mainJPanel.add(addReservationButton);
+        mainJPanel.add(modReservationButton);
+        mainJPanel.add(delReservationButton);
+        mainJPanel.add(retour);
+
+        addReservationButton.addActionListener((e) -> addReservationButtonClicked(e, hotel));
+        modReservationButton.addActionListener((e) -> modReservationButtonClicked(e, hotel));
+        delReservationButton.addActionListener((e) -> delReservationButtonClicked(e, hotel));
+        retour.addActionListener((e) -> nouveauMenu());
+
+    }
+
+    public void vueSejour() {
+        // reset du JPanel
+        mainJPanel.removeAll();
+        mainJPanel.repaint();
+        mainJPanel.revalidate();
+        mainJPanel.setLayout(new GridLayout(4, 1, 5, 40));
+
+        //
         JButton addSejourButton = new JButton("Ajouter Séjour");
-        addSejourButton.addActionListener((e) -> addSejourButtonClicked(e, hotel));
-        mainJPanel.add(addSejourButton);
-        // Extra
         JButton addExtraButton = new JButton("Ajouter Extra");
-        addExtraButton.addActionListener((e) -> addExtraButtonClicked(e, hotel));
+        JButton askFacturatioButton = new JButton("Demander Facturation");
+
+        mainJPanel.add(addSejourButton);
         mainJPanel.add(addExtraButton);
-        // Facturation
-        JButton askFacturationButton = new JButton("Demander Facturation");
-        askFacturationButton.addActionListener((e) -> factureButtonClicked(e, hotel));
-        mainJPanel.add(askFacturationButton);
+        mainJPanel.add(askFacturatioButton);
+        mainJPanel.add(retour);
+
+        addSejourButton.addActionListener((e) -> addSejourButtonClicked(e, hotel));
+        addExtraButton.addActionListener((e) -> addExtraButtonClicked(e, hotel));
+        askFacturatioButton.addActionListener((e) -> factureButtonClicked(e, hotel));
+        retour.addActionListener((e) -> nouveauMenu());
+
+    }
+
+    public void vuePersonnel() {
+        mainJPanel.removeAll();
+        mainJPanel.repaint();
+        mainJPanel.revalidate();
+        mainJPanel.setLayout(new GridLayout(3, 1, 5, 40));
+
+        JButton receptionniste = new JButton("Receptionniste");
+        JButton agent = new JButton("Agent d'entretien");
+
+        mainJPanel.add(receptionniste);
+        mainJPanel.add(agent);
+        mainJPanel.add(retour);
+
+        receptionniste.addActionListener((e) -> vueReceptionniste());
+        agent.addActionListener((e) -> vueAgent());
+        retour.addActionListener((e) -> nouveauMenu());
+
+    }
+
+    public void vueReceptionniste() {
+        mainJPanel.removeAll();
+        mainJPanel.repaint();
+        mainJPanel.revalidate();
+        mainJPanel.setLayout(new GridLayout(3, 1, 5, 40));
+        //
+        JButton addReceptionnisteButton = new JButton("Ajouter Réceptionniste");
+        JButton delReceptionnisteButton = new JButton("Supprimer Réceptionniste");
+
+        mainJPanel.add(addReceptionnisteButton);
+        mainJPanel.add(delReceptionnisteButton);
+        mainJPanel.add(retour);
+
+        addReceptionnisteButton.addActionListener((e) -> addReceptionnisteButtonClicked(e, hotel));
+        delReceptionnisteButton.addActionListener((e) -> delReceptionnisteButtonClicked(e, hotel));
+        retour.addActionListener((e) -> vuePersonnel());
+
+    }
+
+    public void vueAgent() {
+        mainJPanel.removeAll();
+        mainJPanel.repaint();
+        mainJPanel.revalidate();
+        mainJPanel.setLayout(new GridLayout(4, 1, 5, 40));
+        //
+        JButton addAgentButton = new JButton("Ajouter Agent d'entretien");
+        JButton delAgentButton = new JButton("Supprimer Agent d'entretien");
+        JButton addInterButton = new JButton("Ajouter une Intervention");
+
+        mainJPanel.add(addAgentButton);
+        mainJPanel.add(delAgentButton);
+        mainJPanel.add(addInterButton);
+        mainJPanel.add(retour);
+
+        addAgentButton.addActionListener((e) -> addAgentButtonClicked(e, hotel));
+        delAgentButton.addActionListener((e) -> delAgentButtonClicked(e, hotel));
+        addInterButton.addActionListener((e) -> addInterButtonClicked(e, hotel));
+        retour.addActionListener((e) -> vuePersonnel());
+
+    }
+
+    public static void main(String Args[]) {
+        new MainVue();
     }
 }
